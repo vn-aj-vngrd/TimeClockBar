637cbef76f1e6f80814319c657895e3858d84a1d
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2026-07-03 19:46:13 +0800

    Initial Commit

---
+++ TimeClockBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimeClockBar
+//
+//  Created by Van AJ Vanguardia on 7/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimeClockBar/TimeClockBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeClockBarApp.swift
+//  TimeClockBar
+//
+//  Created by Van AJ Vanguardia on 7/3/26.
+//
+
+import SwiftUI
+
+@main
+struct TimeClockBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
